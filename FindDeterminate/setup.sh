4196f6b498030503a7ea2941b795517a09755774
powcoder <powcoder@163.com>
2022-11-22 12:34:51 +0800

    CProgramming

---
+++ FindDeterminate/setup.sh
@@ -0,0 +1,6 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+sudo apt-get update;
+sudo apt-get -y install valgrind;
---
+++ ReadLines/read_lines.h
@@ -0,0 +1,10 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#ifndef READ_LINES
+	#define READ_LINES
+	#include <stdio.h>
+	char* read_line(FILE* fp);
+	void read_lines(FILE* fp, char*** lines, int* num_lines);
+#endif
